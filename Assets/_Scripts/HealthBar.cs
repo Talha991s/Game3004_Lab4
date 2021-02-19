2c06bb982c92d35752224168175ca0fe23bd0304
Talha991s <55731371+Talha991s@users.noreply.github.com>
2021-02-19 02:11:47 -0800

    Added Health Bar

---
+++ Assets/_Scripts/HealthBar.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class HealthBar : MonoBehaviour
+{
+    [Range(0,100)]
+    public int currentHealth =1;
+
+    [Range(1,100)]
+    public int maximunHealth = 1;
+    public Slider healthBarSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBarSlider = GetComponent<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage(10);
+        }
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            Reset();
+        }
+    }
+
+    public void Reset()
+    {
+        healthBarSlider.value = maximunHealth;
+        currentHealth = maximunHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        healthBarSlider.value -= damage;
+        currentHealth -= damage;
+    }
+
+}
